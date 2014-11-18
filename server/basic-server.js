c44e0aa3fe680a764bf7993ddd14076c78330575
tonofprince <brettleibo@gmail.com>
2014-11-18 12:26:54 -0800

    messages being fetched from client

--- server/basic-server.js
+++ server/basic-server.js
@@ -13,28 +13,20 @@ var ip = "127.0.0.1";
 
 var messages = [];
 
-// var defaultCorsHeaders = {
-//   "access-control-allow-origin": "*",
-//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-//   "access-control-allow-headers": "content-type, accept",
-//   "access-control-max-age": 10 // Seconds.
-// };
-// var headers = defaultCorsHeaders;
-// headers['Content-Type '] = 'application/JSON';
-
 app.use(function(req, res, next) {
   res.header("access-control-allow-origin", "*");
   res.header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("access-control-allow-headers", "content-type, accept");
   res.header("access-control-max-age", 10);
+  res.header("content-type", "application/JSON")
   next();
 });
 
-app.get('/classes/:room', function (req, res) {
+app.get('/', function (req, res) {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.end(JSON.stringify({results: messages}));
 });
-app.options('/?order=-createdAt', function (req, res) {
+app.get('/classes/:room', function (req, res) {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.end(JSON.stringify({results: messages}));
 });
