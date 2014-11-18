7ee7bbf84373f2f2c42d26223626ebd678c4022a
tonofprince <brettleibo@gmail.com>
2014-11-17 22:20:19 -0800

    passed all tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -14,43 +14,37 @@ this file and include it in basic-server.js so that it actually works.
 var http = require("http");
 // var path = require("path");
 // var url = require("url");
+var messages = [];
+
 
 var requestHandler = function(request, response) {
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  console.log(request.ondata);
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // var myPath = url.parse(request.url).pathname;
   // var fullPath = path.join(process.cwd(), myPath);
-  var results = [];
-  results.push('apples');
+  // var results = [];
 
   var statusCode = 200;
 
-  if (request.url === '/classes/messages' || request.url === '/classes/room1') {
+  if (request.url === '/classes/messages' || request.url === '/classes/room1' || request.url === '/?order=-createdAt') {
     if (request.method === 'GET') {
       statusCode = 200;
-      results.push('get');
     } else if (request.method === 'POST') {
       statusCode = 201;
-      results.push('post');
+      request.on('data', function(data) {
+        messages.push(JSON.parse(data));
+      });
     }
   } else {
     statusCode = 404;
-    results.push('error');
   }
 
-
   var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "application/JSON";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
+  headers['Content-Type '] = 'application/JSON';
   response.writeHead(statusCode, headers);
+  response.end(JSON.stringify({results: messages}));
 
-
-  response.end(JSON.stringify({results: results}));
 };
 
 exports.requestHandler = requestHandler;
